f01b0a85758475d6953014730d42900272842b47
dan make <danmakes2022@gmail.com>
2019-09-13 10:40:40 -0400

    Creating RestaurantItem component

--- src/components/restaurants/RestaurantItem.js
+++ src/components/restaurants/RestaurantItem.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardContent, Typography } from '@material-ui/core/';
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 800,
+    margin: 10
+  },
+  title: {
+    fontSize: 14
+  }
+});
 const RestaurantItem = ({ restaurant }) => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>{restaurant.name}</h1>
-    </div>
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography className={classes.title} component='h1'>
+          {restaurant.name}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 };
 
